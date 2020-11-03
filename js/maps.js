c48eb8a84e7cdbbed61dde99da866fe1ccb31d7c
Wes Modes <wmodes@gmail.com>
2020-11-02 17:27:44 -0800

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -9,8 +9,8 @@
 var map = L.eeGeo.map('map', 'c37e45fcb636b61e069d29f30f6cb2d6', {
   center: [51.501342, -0.120005],
   zoom: 16,
-  tiltDegrees: 5,
-  tilt: 5,
+  tiltDegrees: 45,
+  tilt: 45,
   indoorsEnabled: false,
   displayEntranceMarkers: false,
 });
@@ -20,3 +20,14 @@ map.themes.setTheme(
     L.eeGeo.themes.time.Dawn,
     L.eeGeo.themes.weather.Snowy
 );
+
+setTimeout(function(){
+  map.setView(
+    [51.501342, -0.120005], 15,
+    {
+        tiltDegrees: 15.0,
+        animate: true,
+        durationSeconds: 4
+      }
+  );
+}, 3000)
