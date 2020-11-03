96f5e2ef315a2e6a19818607a4b74c36ab880f88
Wes Modes <wmodes@gmail.com>
2020-11-02 16:58:44 -0800

    initial commit

---
+++ js/maps.js
@@ -0,0 +1,12 @@
+// var map = L.map('map', {
+//   center: [51.501342, -0.120205],
+//   zoom: 15
+// });
+// L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+var map = L.eeGeo.map('map', 'c37e45fcb636b61e069d29f30f6cb2d6', {
+  center: [51.501342, -0.120005],
+  zoom: 15
+});
