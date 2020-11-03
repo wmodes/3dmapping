ed9a3f67e9c8f69dea83d76abeb40582439a7c80
Wes Modes <wmodes@gmail.com>
2020-11-02 18:21:10 -0800

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -6,35 +6,39 @@
 //   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 // }).addTo(map);
 
-$("#map-wrapper").css("brightness", "1");
+#(document).ready(function(){
 
-var map = L.eeGeo.map('map', 'c37e45fcb636b61e069d29f30f6cb2d6', {
-  center: [51.498976, -0.022702],
-  zoom: 15,
-  tiltDegrees: 45,
-  headingDegrees: 204.374,
-  tilt: 10,
-  tiltDegrees: 10,
-  indoorsEnabled: false,
-  displayEntranceMarkers: false,
-  minZoom: 15
-});
+  $("#map-wrapper").css("brightness", "1");
 
-map.themes.setTheme(
-    L.eeGeo.themes.season.Winter,
-    L.eeGeo.themes.time.Dawn,
-    L.eeGeo.themes.weather.Snowy
-);
+  var map = L.eeGeo.map('map', 'c37e45fcb636b61e069d29f30f6cb2d6', {
+    center: [51.498976, -0.022702],
+    zoom: 15,
+    tiltDegrees: 45,
+    headingDegrees: 204.374,
+    tilt: 10,
+    tiltDegrees: 10,
+    indoorsEnabled: false,
+    displayEntranceMarkers: false,
+    minZoom: 15
+  });
 
-setTimeout(function(){
-  console.log("Changing POV");
-  map.setView(
-    [51.501342, -0.120005], 16.25,
-    {
-        tiltDegrees: 5,
-        headingDegrees: 0,
-        animate: true,
-        durationSeconds: 20
-      }
+  map.themes.setTheme(
+      L.eeGeo.themes.season.Winter,
+      L.eeGeo.themes.time.Dawn,
+      L.eeGeo.themes.weather.Snowy
   );
-}, 10000);
+
+  setTimeout(function(){
+    console.log("Changing POV");
+    map.setView(
+      [51.501342, -0.120005], 16.25,
+      {
+          tiltDegrees: 5,
+          headingDegrees: 0,
+          animate: true,
+          durationSeconds: 20
+        }
+    );
+  }, 10000);
+
+})
