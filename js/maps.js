4d07c67fb001cca13cc328631222f06342590004
Wes Modes <wmodes@gmail.com>
2020-11-02 17:19:06 -0800

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -8,9 +8,9 @@
 
 var map = L.eeGeo.map('map', 'c37e45fcb636b61e069d29f30f6cb2d6', {
   center: [51.501342, -0.120005],
-  zoom: 15,
+  zoom: 16,
   tiltDegrees: 5,
-  tilt: 10,
+  tilt: 5,
   indoorsEnabled: false,
   displayEntranceMarkers: false,
 });
