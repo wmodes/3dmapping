55bd27678c45e9edb7b32a17c17fd7151636f84a
Wes Modes <wmodes@gmail.com>
2020-11-03 08:08:25 -0800

    Update newyork.js

--- js/newyork.js
+++ js/newyork.js
@@ -38,7 +38,7 @@ $(document).ready(function(){
           tilt: 5,
           headingDegrees: 27,
           animate: true,
-          durationSeconds: 180
+          durationSeconds: 120
         }
     );
   }, 2000);
