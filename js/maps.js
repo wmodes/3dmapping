30d2cc4ed008d1b6303df3c663ea16032f78179a
Wes Modes <wmodes@gmail.com>
2020-11-02 17:36:04 -0800

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -8,8 +8,9 @@
 
 var map = L.eeGeo.map('map', 'c37e45fcb636b61e069d29f30f6cb2d6', {
   center: [51.501342, -0.120005],
-  zoom: 15,
+  zoom: 14,
   tiltDegrees: 45,
+  headingDegrees: 204.374,
   tilt: 45,
   indoorsEnabled: false,
   displayEntranceMarkers: false,
@@ -24,10 +25,10 @@ map.themes.setTheme(
 setTimeout(function(){
   console.log("Changing POV");
   map.setView(
-    [51.501342, -0.120005], 17,
+    [51.501342, -0.120005], 16,
     {
         tiltDegrees: 85,
-        headingDegrees: 204.374,
+        headingDegrees: 0,
         animate: true,
         durationSeconds: 8
       }
